20b23f38ae637aa0841c92c86f4a7c2a9ee9dcb3
devbens <benjophp@gmail.com>
2022-07-12 06:49:33 +0000

    first commit

---
+++ init/starts-initial-check.sh
@@ -0,0 +1,49 @@
+#-----------------------------------------------------------------------
+#
+# Base functions
+#
+# The base functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   base <benjophp@gmail.com>
+#
+# Copyright base
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger scripts!
+#
+# Base - https://github.com/inveks/base
+#
+#-----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check initial setup
+#
+# You must inform the parameters below:
+# 1. [optional] (default: ) Pid file name
+# 2. [optional] (default: false) Check if there is an .env file in a
+# folder up (./../.env) and 'source' it
+#
+# ----------------------------------------------------------------------
+starts_initial_check()
+{
+    local LOCAL_PID_FILE LOCAL_CHECK_BASE_ENV_FILE
+
+    LOCAL_PID_FILE=${1:-$PID_FILE}
+    LOCAL_CHECK_BASE_ENV_FILE=${2:-false}
+
+    # Check if docker is installed
+    run_function check_docker
+
+    if [[ "$LOCAL_CHECK_BASE_ENV_FILE" == true ]]; then
+      # Check if there is an .env file in base folder
+      run_function check_base_env_file
+    fi
+
+    # Check if you are already running an instance of this Script
+    run_function check_running_script $LOCAL_PID_FILE
+}
